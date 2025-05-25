f7fdc36c33d7d5f81840b9e7b7dcd40155cd2b67
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2025-05-25 03:15:34 -0300

    create Repositories

---
+++ Blog/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace Blog.Models;
+
+[Table("[role]")]
+public class Role
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Slug { get; set; }
+}
